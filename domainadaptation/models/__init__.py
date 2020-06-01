a75d34508126567bd90afa8ad5a9885d02ccd0ac
ekolodin <ekolodin98@gmail.com>
2020-06-01 16:06:31 +0300

    update models

--- domainadaptation/models/__init__.py
+++ domainadaptation/models/__init__.py
@@ -1,4 +1,4 @@
 from domainadaptation.models.blocks import GradientReversal
-from domainadaptation.models.alexnet import AlexNet, proprocess_input
+from domainadaptation.models.alexnet import AlexNet, preprocess_input
 
 __all__ = ['GradientReversal', 'AlexNet']
